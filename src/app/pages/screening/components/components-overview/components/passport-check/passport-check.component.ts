e390f729622d8430960c97d11fc60500894dbda5
adarsh-aman <aman.rishu98@gmail.com>
2020-03-13 11:40:13 +0530

    passport check api integration

--- src/app/pages/screening/components/components-overview/components/passport-check/passport-check.component.ts
+++ src/app/pages/screening/components/components-overview/components/passport-check/passport-check.component.ts
@@ -9,6 +9,8 @@ import icVisibility from '@iconify/icons-ic/twotone-visibility';
 import icVisibilityOff from '@iconify/icons-ic/twotone-visibility-off';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiProvider } from 'src/app/services/api-provider';
+
 
 @Component({
   selector: 'vex-passport-check',
@@ -23,15 +25,18 @@ export class PassportcheckComponent implements OnInit {
 <mat-progress-bar mode="buffer" color="accent"></mat-progress-bar>
 <mat-progress-bar mode="query" color="warn"></mat-progress-bar>`;
 
-  public componentsoverviewProgress: any;
+  public passportCheckForm: any;
     submitted = false;
 
 
- constructor(private router: Router, private formBuilder: FormBuilder) {}
+ constructor(
+   private router: Router,
+    private formBuilder: FormBuilder,
+    private apiProvider: ApiProvider) {}
  
 
    ngOnInit() {
-   this.componentsoverviewProgress = this.formBuilder.group({
+   this.passportCheckForm = this.formBuilder.group({
 	
 	  Name: [null, Validators.compose([Validators.required])],
 	  caseId: [null, Validators.compose([Validators.required])],
@@ -50,14 +55,41 @@ export class PassportcheckComponent implements OnInit {
 
 
  public hasError = (controlName: string, errorName: string) => {
-    return this.componentsoverviewProgress.controls[controlName].hasError(errorName);
+    return this.passportCheckForm.controls[controlName].hasError(errorName);
   }
-  componentsoverviewOnsubmit(){
-    this.submitted = true;
-    if (this.componentsoverviewProgress.invalid) {
-      return;
+  submitPassportCheck(){
+    // this.submitted = true;
+    // if (this.passportCheckForm.invalid) {
+    //   return;
+    // }
+    // alert('form fields are validated successfully!');  
+    const inputData = {
+      // name: this.vesselScreeningForm.value.name,
+      // caseId: this.vesselScreeningForm.value.caseId,
+      // registeredCountry: this.vesselScreeningForm.value.registeredCountry
+      Name: this.passportCheckForm.value.Name,
+      caseId: this.passportCheckForm.value.caseId,
+      date: this.passportCheckForm.value.date,
+      countryLoc: this.passportCheckForm.value.countryLoc,
+      placeofBirth: this.passportCheckForm.value.placeofBirth,
+      Nationality: this.passportCheckForm.value.Nationality,
+      givenName: this.passportCheckForm.value.givenName,
+      lastName: this.passportCheckForm.value.lastName,
+      issuingState: this.passportCheckForm.value.issuingState,
+      passportcheckNat: this.passportCheckForm.value.passportcheckNat,
+      idNumber: this.passportCheckForm.value.idNumber,
+      dateofExpiry: this.passportCheckForm.value.dateofExpiry
     }
-    alert('form fields are validated successfully!');  
+    console.log({inputData})
+    this.apiProvider.createPassportCheck('createPassportCheck',inputData).subscribe(
+      async resdata => {
+                const res = resdata;
+                if(res){
+                  //show some message
+                }
+        }, async (error) => {
+          console.log("error occured")
+        });
   }
 
 }
