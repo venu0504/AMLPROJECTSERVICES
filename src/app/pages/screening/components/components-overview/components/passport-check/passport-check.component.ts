5e2dcaf343035ee9b87df9d00887dd30b0e04e17
adarsh-aman <aman.rishu98@gmail.com>
2020-04-04 02:07:34 +0530

    Changes for passport check screening | api integration

--- src/app/pages/screening/components/components-overview/components/passport-check/passport-check.component.ts
+++ src/app/pages/screening/components/components-overview/components/passport-check/passport-check.component.ts
@@ -29,6 +29,7 @@ export class PassportcheckComponent implements OnInit {
 
   public passportCheckForm: any;
     submitted = false;
+    public countryList:Array<Object> = [];
 
 
  constructor(
@@ -40,7 +41,6 @@ export class PassportcheckComponent implements OnInit {
 
    ngOnInit() {
    this.passportCheckForm = this.formBuilder.group({
-	
 	  Name: [null, Validators.compose([Validators.required])],
 	  caseId: [null, Validators.compose([Validators.required])],
 	  date: [null, Validators.compose([Validators.required])],
@@ -54,6 +54,17 @@ export class PassportcheckComponent implements OnInit {
 	  idNumber: [null, Validators.compose([Validators.required])],
 	  dateofExpiry: [null, Validators.compose([Validators.required])]
  });
+ this.ComponentsOverviewSVC.getCountryList('reference/countries').subscribe(
+  async resdata => {
+            const res = resdata;
+            if(res){
+              console.log('res country',res)
+              this.countryList = res;
+            }
+    }, async (error) => {
+      console.log("error occured")
+    });
+ 
   }
 
 
@@ -66,25 +77,48 @@ export class PassportcheckComponent implements OnInit {
     //   return;
     // }
     // alert('form fields are validated successfully!');  
+    // const inputData = {
+    //   // name: this.vesselScreeningForm.value.name,
+    //   // caseId: this.vesselScreeningForm.value.caseId,
+    //   // registeredCountry: this.vesselScreeningForm.value.registeredCountry
+    //   Name: this.passportCheckForm.value.Name,
+    //   caseId: this.passportCheckForm.value.caseId,
+    //   date: this.passportCheckForm.value.date,
+    //   countryLoc: this.passportCheckForm.value.countryLoc,
+    //   placeofBirth: this.passportCheckForm.value.placeofBirth,
+    //   Nationality: this.passportCheckForm.value.Nationality,
+    //   givenName: this.passportCheckForm.value.givenName,
+    //   lastName: this.passportCheckForm.value.lastName,
+    //   issuingState: this.passportCheckForm.value.issuingState,
+    //   passportcheckNat: this.passportCheckForm.value.passportcheckNat,
+    //   idNumber: this.passportCheckForm.value.idNumber,
+    //   dateofExpiry: this.passportCheckForm.value.dateofExpiry
+    // }
     const inputData = {
-      // name: this.vesselScreeningForm.value.name,
-      // caseId: this.vesselScreeningForm.value.caseId,
-      // registeredCountry: this.vesselScreeningForm.value.registeredCountry
-      Name: this.passportCheckForm.value.Name,
-      caseId: this.passportCheckForm.value.caseId,
-      date: this.passportCheckForm.value.date,
-      countryLoc: this.passportCheckForm.value.countryLoc,
-      placeofBirth: this.passportCheckForm.value.placeofBirth,
-      Nationality: this.passportCheckForm.value.Nationality,
-      givenName: this.passportCheckForm.value.givenName,
-      lastName: this.passportCheckForm.value.lastName,
-      issuingState: this.passportCheckForm.value.issuingState,
-      passportcheckNat: this.passportCheckForm.value.passportcheckNat,
-      idNumber: this.passportCheckForm.value.idNumber,
-      dateofExpiry: this.passportCheckForm.value.dateofExpiry
+      groupId: localStorage.getItem('groupId'),
+      entityType: 'INDIVIDUAL',
+      providerTypes: [
+            "WATCHLIST", "PASSPORT_CHECK"
+          ],
+      clientCaseId:"",
+      name: this.passportCheckForm.value.name,
+      caseScreeningState:"INITIAL",
+      secondaryFields:[
+        {typeId: 'SFCT_4',value: this.passportCheckForm.value.date},
+        {typeId: 'SFCT_5',value: this.passportCheckForm.value.countryLoc},
+        {typeId: 'SFCT_6',value: this.passportCheckForm.value.placeofBirth},
+        {typeId: 'SFCT_7',value:  this.passportCheckForm.value.Nationality},
+        {typeId: 'SFCT_8',value: this.passportCheckForm.value.givenName},
+        {typeId: 'SFCT_9',value: this.passportCheckForm.value.lastName},
+        {typeId: 'SFCT_10',value: this.passportCheckForm.value.issuingState},
+        {typeId: 'SFCT_11',value: this.passportCheckForm.value.passportcheckNat},
+        {typeId: 'SFCT_12',value: this.passportCheckForm.value.idNumber},
+        {typeId: 'SFCT_13',value: this.passportCheckForm.value.dateofExpiry},
+        ],
+        customFields:[]
     }
     console.log({inputData})
-    this.ComponentsOverviewSVC.createPassportCheck('createPassportCheck',inputData).subscribe(
+    this.ComponentsOverviewSVC.createPassportCheck('cases',inputData).subscribe(
       async resdata => {
                 const res = resdata;
                 if(res){
